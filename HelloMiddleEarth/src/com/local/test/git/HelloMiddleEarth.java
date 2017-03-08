e616ffe5a114c57a395166426cf93b41ce868063
HawaiiBill <w_ek@yahoo.com>
2017-03-08 07:29:32 -0800

    Initial check-in.

---
+++ HelloMiddleEarth/src/com/local/test/git/HelloMiddleEarth.java
@@ -0,0 +1,16 @@
+package com.local.test.git;
+
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+		
+		String[] names = {"Bilbo", "Frodo", "Pipin", "Gandalf"};
+		
+		for (String name : names) {
+			System.out.println("Hello " + name);
+		}
+		
+	}
+
+}
